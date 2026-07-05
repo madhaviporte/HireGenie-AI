398cd8a7147545a2fdccaaa859411fb632837632
madhaviporte <madhaviporte2004@gmail.com>
2026-07-05 14:07:11 +0530

    server setup

---
+++ Backend/models/user.model.js
@@ -0,0 +1,23 @@
+import { request } from "express";
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+name:{
+    type:String,
+    required:true 
+},
+email:{
+    type:String,
+    unique:true,
+    request:true
+},
+credits:{
+    type:Number,
+    default:100
+}
+
+},{timeseries:true})
+
+const User = mongoose.model("User", userSchema)
+
+export default User
