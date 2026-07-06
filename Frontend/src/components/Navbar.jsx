f1fe186faf22cb706b2f18fb9ae8994acb6a7886
madhaviporte <madhaviporte2004@gmail.com>
2026-07-07 02:16:53 +0530

    complete home page and navigate interview and history page

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { ServerUrl } from '../App'
 import { setUserData } from '../redux/userSlice'
+import AuthModel from './AuthModel'
 
 const Navbar = () => {
     const {userData} = useSelector((state)=> state.user)
@@ -15,6 +16,7 @@ const Navbar = () => {
     const [showUserPopup,setShowUserPopup] = useState(false)
     const navigate = useNavigate()
     const dispatch = useDispatch()
+    const [showAuth, setShowAuth] = useState(false)
 
     const handleLogout = async () => {
         try {
@@ -23,7 +25,7 @@ const Navbar = () => {
             dispatch(setUserData(null))
             setShowCreditPopup(false)
             setShowUserPopup(false)
-            nevigate("/")
+            navigate("/")
 
         } catch (error) {
             console.log(error);
@@ -50,8 +52,12 @@ const Navbar = () => {
 <div className='flex items-center gap-6 relative'>
 
    <div className='relative'>
-    <button onClick={()=>{setShowCreditPopup
-(!showCreditPopup)
+    <button onClick={()=>{
+        if(!userData){
+            setShowAuth(true)
+            return;
+        }
+        setShowCreditPopup(!showCreditPopup)
   setShowUserPopup(false)
 }} 
         className='flex items-center gap-2 bg-gray-100 px-4 py-2 rounded-full text-md hover:bg-gray-200 transition'>
@@ -71,7 +77,12 @@ const Navbar = () => {
 
     <div className='relative'>
     <button 
-    onClick={()=>{setShowUserPopup(!showUserPopup);
+    onClick={()=>{
+         if(!userData){
+            setShowAuth(true)
+            return;
+        }
+        setShowUserPopup(!showUserPopup);
         setShowCreditPopup(false)
     }}
     className='w-9 h-9 bg-black text-white rounded-full flex items-center justify-center font-semibold'>
@@ -83,10 +94,10 @@ const Navbar = () => {
             <p className='text-md text-blue-500 font-medium mb-1'>{userData?.name}</p>
 
             <button
-            onClick={()=>nevigate("/history")}
+            onClick={()=>navigate("/history")}
             className='w-full text-left text-sm py-2 hover:text-black text-gray-600'>InterView History</button>
             <button 
-            onClick={(handleLogout)}
+            onClick={handleLogout}
             className='w-full test-left text-sm py-2 flex items-center gap-2 text-red-500'>
                 <HiOutlineLogout size={16}/>
                 Logout</button>
@@ -97,6 +108,8 @@ const Navbar = () => {
     </div>
 
       </motion.div>
+
+      {showAuth && <AuthModel onClose={()=>setShowAuth(false)}/>}
     </div>
   )
 }
