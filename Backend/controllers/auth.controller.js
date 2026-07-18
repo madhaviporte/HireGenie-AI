a7be15ec64b178fca6cabdfae2cdaa5a3143a4f5
madhaviporte <madhaviporte2004@gmail.com>
2026-07-18 12:47:44 +0530

    cookies issue solve

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -13,12 +13,11 @@ export const googleAuth = async (req,res) => {
             })
         }
         let token = await genToken(user._id)
-        res.cookie("token" , token , {
-            http:true,
-            secure:true,
-            sameSide:"none",
-            maxAge: 7 * 24 * 60 * 60 * 1000
-        })
+        res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+});
 
         return res.status(200).json(user)
 
